fec5e9714720dd693fc61ded8ae2cd81fabfa0b3
gene5ive <jedigene@gmail.com>
2015-08-07 16:33:41 -0700

    represent code up to page 75 of afformentioned PDF

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,12 +1,42 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: "tr",
-    template: "#contact-list-item"
+    template: "#contact-list-item",
+
+    events: {
+      "click": "highlightName",
+      "click": "printModel",
+      "click button.js-delete": "deleteClicked"
+    },
+
+    highlightName: function(e){
+      e.preventDefault();
+      this.$el.toggleClass("warning");
+    },
+
+    printModel: function(e){
+      e.preventDefault();
+      console.log("Highlighting toggled on model: ", this.model)
+    },
+
+    deleteClicked: function(e){
+      e.stopPropagation();
+      this.trigger("contact:delete", this.model);
+    },
+
+    remove: function(){
+      var self = this;
+      this.$el.fadeOut(function(){
+        Marionette.ItemView.prototype.remove.call(self);
+      });
+    }
   });
 
-  List.Contacts = Marionette.CollectionView.extend({
+  List.Contacts = Marionette.CompositeView.extend({
     tagName: "table",
     className: "table table-hover",
-    childView: List.Contact
+    template: "#contact-list",
+    childView: List.Contact,
+    childViewContainer: "tbody"
   });
 });
